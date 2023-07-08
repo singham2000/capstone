ef82c3ea6c988c7fb7640769fe168f2970e21b81
surya pratap <thakursuryapratap9@gmail.com>
2023-07-08 15:04:52 +0530

    Project Init and basic structure

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Componenets/Navbar";
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar />
+      Homepage
+    </div>
+  );
+};
+
+export default Homepage;
