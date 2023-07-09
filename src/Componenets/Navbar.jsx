05b8d2ce2697e9c1eb71be6c5154f5f78450485a
surya pratap <thakursuryapratap9@gmail.com>
2023-07-09 17:17:58 +0530

    error fixes

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="drop-shadow-md min-w-full bg-gradient-to-r from-orange-500 text-white h-12 text-center flex flex-row items-center px-5 justify-between">
-        <h1 className="text_openSans">CapStone</h1>
+      <div className="drop-shadow-md min-w-full bg-gradient-to-r from-red-500 text-white h-12 text-center flex flex-row items-center px-5 justify-between">
+        <h1 className="text_openSans font-extrabold">CapStone</h1>
         <div>
           <ul className="flex flex-row gap-2 text-orange-700">
             <li>Home</li>
-            <li className="animate-bounce">Register</li>
+            <li className="">Register</li>
             <li>Help</li>
           </ul>
         </div>
