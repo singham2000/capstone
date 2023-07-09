d0cfeb007ecb8366e3039433ff6574b62027277b
surya pratap <thakursuryapratap9@gmail.com>
2023-07-09 16:48:41 +0530

    TailwindCss and React Router Installation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Homepage from "./Pages/Homepage";
+import './App.css';
 function App() {
   return (
     <div className="App">
--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="drop-shadow-md min-w-full bg-gradient-to-r from-orange-500 text-white h-12 text-center flex flex-row items-center px-5 justify-between">
+        <h1 className="text_openSans">CapStone</h1>
+        <div>
+          <ul className="flex flex-row gap-2 text-orange-700">
+            <li>Home</li>
+            <li className="animate-bounce">Register</li>
+            <li>Help</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
