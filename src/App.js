d885f7d2405caaca0ef1f60e0bbb7cbf179e0181
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2023-08-28 15:42:26 +0000

    Navbar addded at top and /signup route added

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import RegisterRestro from "./Pages/RegisterRestro";
 import Login from "./Pages/Login";
+import Navbar from "./Componenets/Navbar";
+import Signup from "./Pages/Signup";
 
 //CSS import
 import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/registerRestro" element={<RegisterRestro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
