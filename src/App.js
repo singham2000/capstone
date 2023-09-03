482fb5d5484b40bd21e3093c66cf1af54fe04727
singham2000 <thakursuryapratap9@gmail.com>
2023-09-03 23:11:47 +0530

    Routes Added for restro Listing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //pages Import
 import Homepage from "./Pages/Homepage";
 import RegisterRestro from "./Pages/RegisterRestro";
+import RestroListing from './Pages/RestroListing';
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+
+// Components imports 
 import Navbar from "./Componenets/Navbar";
 import Footer from "./Componenets/Footer";
 
@@ -18,6 +21,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/registerRestro" element={<RegisterRestro />} />
+        <Route path="/restroList" element={<RestroListing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
