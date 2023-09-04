d50f139b209cd00c4c3b03df0e5c1560fd738200
singham2000 <thakursuryapratap9@gmail.com>
2023-09-04 13:51:30 +0530

    Restro listing page

--- src/Pages/RestroListing.jsx
+++ src/Pages/RestroListing.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Restaurant from '../Componenets/Restaurant'
 
 const RestroListing = () => {
     return (
         <div>
-            <div>
-                Best Restaurants
+            <div className="banner banner1 flex justify-center items-center">
+                <h1 className="text-white font-bold text-9xl max-sm:text-6xl">Restaurants</h1>
+            </div>
+            <div className='flex justify-center items-center m-4'>
+                <Restaurant />
             </div>
         </div>
     )
